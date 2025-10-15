ae59cb8c10b61cf61956feb08821436194b925d1
oak viviz <viviz.thapunyaphat@allianz.com>
2025-10-15 14:30:55 +0700

    d

--- src/app/app.ts
+++ src/app/app.ts
@@ -55,47 +55,4 @@ import { NxSmallStageModule } from '@allianz/ng-aquila/small-stage';
 })
 export class App {
   protected readonly title = signal('aquila-starter-app');
-
-  @ViewChild('consentTemplate')
-  consentTemplateRef!: TemplateRef<any>;
-
-  @ViewChild('submitTemplate')
-  submitTemplateRef!: TemplateRef<any>;
-
-  dialogRef?: NxModalRef<any, any>;
-
-  formGroup: FormGroup;
-
-  constructor(public dialogService: NxDialogService) {
-    this.formGroup = new FormBuilder().group({
-      name: ['', Validators.required],
-      items: ['', Validators.required],
-      email: ['', [Validators.email, Validators.required]],
-      consent: [false, Validators.requiredTrue],
-    });
-  }
-
-  openConsentDialog(): void {
-    this.dialogRef = this.dialogService.open(this.consentTemplateRef, {
-      ariaLabel: 'A modal with content',
-      showCloseIcon: true,
-    });
-  }
-
-  openSubmitDialog(): void {
-    this.dialogRef = this.dialogService.open(this.submitTemplateRef, {
-      ariaLabel: 'The final modal of the Starter App',
-      showCloseIcon: false,
-    });
-  }
-
-  closeDialog() {
-    this.dialogRef?.close();
-  }
-
-  onSubmit() {
-    if (this.formGroup.valid) {
-      this.openSubmitDialog();
-    }
-  }
 }
