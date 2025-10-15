4403984a5a03375bade2d2b7d712fba8ee615fbd
oak viviz <viviz.thapunyaphat@allianz.com>
2025-10-15 13:53:20 +0700

    s

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,12 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes), provideAnimations()
   ]
 };
